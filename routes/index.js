de15cdfa334d7fb3ab67de3c5f6996167c51343a
IronZaphilith <maciek_st@o2.pl>
2020-09-04 15:53:15 +0200

    Signing form fixed

--- routes/index.js
+++ routes/index.js
@@ -4,51 +4,19 @@ const fs = require('fs');
 const path = require("path");
 const router = express.Router();
 const axios = require('axios');
-const utf8 = require('utf8');
 const FormData = require('form-data');
-var aws4  = require('aws4')
-
-
-function generateEncodedPolicy(currentDate) {
-  const expiryDate = new Date(currentDate);
-  expiryDate.setHours(new Date().getHours() + 1);
-  let policy = {
-    expiration: expiryDate.toISOString(),
-    conditions: [
-      {"bucket": "mstokfisz-to-process"},
-      {"acl": "public-read"},
-      ["starts-with", "$Content-Type", "image/"],
-      {"x-amz-server-side-encryption": "AES256"},
-      {"x-amz-credential": "ASIAYLZJICDARW34EVWV/"+currentDate.toISOString().slice(0,10).replace(/-/g,"")+"/us-east-1/s3/aws4_request"},
-      {"x-amz-algorithm": "AWS4-HMAC-SHA256"},
-      {"x-amz-date": currentDate.toISOString() }
-    ]
-  };
-  return new Buffer(utf8.encode(JSON.stringify(policy))).toString('base64');
-}
-
-const sendForm = function (fileName, policy, signature, currentDate) {
-  let formData = new FormData();
-  formData.append("acl", "public-read");
-  formData.append("key", fileName);
-  formData.append("policy", policy);
-  formData.append("x-amz-algorithm", "AWS4-HMAC-SHA256");
-  formData.append("x-amz-credential", "ASIAYLZJICDARW34EVWV/"+currentDate.toISOString().slice(0,10).replace(/-/g,"")+"/us-east-1/s3/aws4_request");
-  formData.append("x-amz-date", currentDate.toISOString().slice(0,19).replace(/[-:]/g, "") + "Z");
-  formData.append("x-amz-signature", signature);
-  formData.append("file", fs.readFileSync(path.join(__dirname, "./tmp/"+fileName), { encoding: 'base64' }), fileName);
-
-  console.log(currentDate.toISOString());
-  var opts = {headers: formData.getHeaders()};
-
-  axios.post("http://mstokfisz-to-process.s3.amazonaws.com/", formData, {headers: formData.getHeaders()}).catch((err) => {
-    console.log(err);
-  });
-}
 
 const upload = multer({
   dest: path.join(__dirname, "./tmp")
 });
+
+function createForm(fields) {
+  let form_data = new FormData();
+  for (let key in fields) {
+    form_data.append(key, fields[key]);
+  }
+  return form_data;
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -62,19 +30,25 @@ async function (req, res) {
   fs.rename(tempPath, path.join(__dirname, "./tmp/"+req.file.originalname), err => {
     if (err) return handleError(err, res);
   });
-  const currentDate = new Date();
-  let signature = "";
-  const policy = generateEncodedPolicy(currentDate);
-  await axios.post('http://localhost:5000/get-signature', {
-    policy,
-    date: currentDate.toISOString().slice(0,10).replace(/-/g,""),
-    region: 'us-east-1'
+  let form;
+  await axios.post('http://localhost:5000/signed-form-upload', {
+    filename: req.file.originalname
   }).then(res => {
-    signature = res.data.signature;
+    console.log("Test");
+    form = res.data;
   }).catch(err => {
     console.log(err);
   });
-  sendForm(req.file.originalname, policy, signature, currentDate);
+  const form_data = createForm(form.fields);
+  // form_data.append('Success_Action_Status', '201');
+  form_data.append('file', fs.createReadStream(path.join(__dirname, "./tmp/"+req.file.originalname)));
+  console.log(form_data);
+  form_data.submit("https://images-to-process-mstokfisz.s3.amazonaws.com", function(err, res) {
+    console.log(res.statusCode + " " + res.statusMessage);
+    if (err) {
+      console.log("ERROR:\n"+err);
+    }
+  });
 });
 
 module.exports = router;
